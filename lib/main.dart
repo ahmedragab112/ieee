cd7dd9991bb048af436e84b389167a721277ac6a
ahmedragab <ar1755@fayoum.edu.eg>
2024-02-05 20:39:18 +0200

    explain some widget

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Test());
+}
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Text('IEEE'),
+          centerTitle: true,
+          leading: const Icon(Icons.abc),
+          actions: const [
+            Icon(Icons.abc),
+            SizedBox(
+              width: 100,
+            ),
+            Icon(Icons.abc)
+          ],
+        ),
+        body: const Center(child: Text('Hello, World!')),
+      ),
+    );
+  }
+}
