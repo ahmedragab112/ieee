0c24a933e93623ca676deb964c10ccc7cd25e4be
ahmedragab <ar1755@fayoum.edu.eg>
2024-02-09 18:24:05 +0200

    setion 2

---
+++ lib/body.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topLeft,
+      children: [
+        Container(
+          width: 200,
+          height: 200,
+          color: Colors.red,
+        ),
+        Container(
+          width: 150,
+          height: 150,
+          color: Colors.blue,
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          color: Colors.white,
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ieee/body.dart';
 
 void main() {
   runApp(const Test());
@@ -17,15 +18,16 @@ class Test extends StatelessWidget {
           title: const Text('ahmed ragab'),
           centerTitle: true,
           leading: const Icon(Icons.abc),
-          actions: const [
-            Icon(Icons.abc), 
-            SizedBox(
-              width: 100,
-            ),
-            Icon(Icons.abc)
-          ],
+          actions: const [Icon(Icons.abc), Icon(Icons.abc)],
         ),
-        body: const Center(child: Text('Hello, World!')),
+        body: const Body(),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+              size: 30,
+            )),
       ),
     );
   }
